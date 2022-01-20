8713aaf0c58895899ce92d6a4007b6a1ef820560
CharlotteAndreasson <charlotte.andreasson@student.kyh.se>
2022-01-20 12:10:34 +0100

    first commit

---
+++ models.py
@@ -0,0 +1,22 @@
+
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150))
+    email = db.Column(db.String(150), unique=True) # Vill inte ha två användare med samma epost
+    password = db.Column(db.String(100))
+    admin = db.Column(db.BOOLEAN, default=False)
+
+    def is_active(self):
+        return True
+
+    def is_authenticated(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id # Man kan också returnera email här
